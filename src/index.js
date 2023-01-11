fc6936142815559e6188d86fff68de264f68fb12
karmeowwoof <karmeowwoof@gmail.com>
2023-01-11 13:52:21 +0300

    strnge problem with tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// Импортируйте наш пользовательский CSS
 import './styles/style.scss'
-// Импортируйте весь JS Bootstrap
 import 'bootstrap/dist/js/bootstrap.min.js';
+
 import runApp  from './app.js';
 
 runApp();
